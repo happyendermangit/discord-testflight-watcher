66df179c4ced31988b32ba4fa08bd102e67e99fd
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2024-03-15 13:41:55 +0000

    Create main.js

---
+++ main.js
@@ -0,0 +1,37 @@
+async function main(){
+  const content = await (await fetch("https://testflight.apple.com/join/gdE4pRzI")).text()
+  const webhook = "https://canary.discord.com/api/webhooks/1218190715910356992/8DP3Ka0j4iX8kXUroDRwRSb1E73Jo0b1zF3uJ1zkn6AR7-NhfQ2MWsbGVbmkf8aKoFKR"
+  
+  if (content.includes("This beta is full.")){
+    // full
+         await fetch(webhook, {
+      method: "POST",
+      body: JSON.stringify({
+        content: "Testflight is **full**!"
+    })
+    })
+    return 
+  }
+  if (content.includes("This beta isn't accepting"){
+    // closed
+     await fetch(webhook, {
+      method: "POST",
+      body: JSON.stringify({
+        content: "Testflight is **closed**!"
+    })
+    })
+  
+    return   
+  }
+
+  // open 
+  await fetch(webhook, {
+    method: "POST",
+    body: JSON.stringify({
+      content: `${role} Testflight is now **open**!`
+    })
+  })
+  
+  
+}
+main()
