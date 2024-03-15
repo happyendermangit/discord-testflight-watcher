8ef27f1a41e2850c49039eeb6033ec53e843004f
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2024-03-15 13:44:31 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 async function main(){
   const content = await (await fetch("https://testflight.apple.com/join/gdE4pRzI")).text()
   const webhook = "https://canary.discord.com/api/webhooks/1218190715910356992/8DP3Ka0j4iX8kXUroDRwRSb1E73Jo0b1zF3uJ1zkn6AR7-NhfQ2MWsbGVbmkf8aKoFKR"
+  const role = "<@&1218192974979137607>"
+
   
   if (content.includes("This beta is full.")){
     // full
